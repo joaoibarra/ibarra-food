2006768906df5ac23915ab5e0913a778d15927bd
Joao Ibarra <joao@anyplay.audio>
2021-02-28 11:54:33 +0100

    Adding basic config

---
+++ app/src/main/java/com/joaoibarra/food/di/DatabaseModule.kt
@@ -0,0 +1,6 @@
+package com.joaoibarra.food.di
+
+import org.koin.dsl.module
+
+val DatabaseModule = module {
+}
---
+++ app/src/main/java/com/joaoibarra/food/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.joaoibarra.food.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joaoibarra.food.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
