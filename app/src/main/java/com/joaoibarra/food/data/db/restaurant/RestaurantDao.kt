cc09139ac209c10653c6a1938e143d7a177de3db
Joao Ibarra <joao@anyplay.audio>
2021-03-07 19:53:58 +0100

    Search by name

--- app/src/main/java/com/joaoibarra/food/data/db/restaurant/RestaurantDao.kt
+++ app/src/main/java/com/joaoibarra/food/data/db/restaurant/RestaurantDao.kt
@@ -5,14 +5,17 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RestaurantDao {
-    @Query("SELECT * FROM restaurant")
-    fun getAll(): Flow<List<Restaurant>>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertOrUpdate(restaurant: Restaurant)
+    @Query("SELECT * FROM restaurant WHERE name LIKE '%' || :keyword || '%' ORDER BY isFavorite DESC, name ASC, distance ASC")
+    fun getAll(keyword: String = "") : Flow<List<Restaurant>>
 
     @Query("SELECT * FROM restaurant WHERE id LIKE :restaurantId")
     fun findById(restaurantId: Int): Restaurant?
+
+    @Query("SELECT COUNT() FROM restaurant")
+    fun getCount() : Int
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrUpdate(restaurant: Restaurant)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertList(restaurant: List<Restaurant>)
--- app/src/main/java/com/joaoibarra/food/ui/restaurant/RestaurantRepository.kt
+++ app/src/main/java/com/joaoibarra/food/ui/restaurant/RestaurantRepository.kt
@@ -4,38 +4,36 @@ import com.joaoibarra.food.data.db.restaurant.Restaurant
 import com.joaoibarra.food.data.db.restaurant.RestaurantDao
 import com.joaoibarra.food.data.remote.IbarraFoodApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.withContext
 
 class RestaurantRepository(
         private val ibarraFoodApi: IbarraFoodApi,
         private val restaurantDao: RestaurantDao
 ) {
-    fun getRestaurants() = flow<List<Restaurant>> {
-        restaurantDao.getAll().collect {
-            if (it.isNotEmpty()) {
-                emit(it)
-            } else {
-                getRestaurantsFromRemote().collect {
-                    emit(it)
-                }
-            }
+    fun getRestaurants(keyword: String = "") = flow {
+        currentCoroutineContext().ensureActive()
+        if (restaurantDao.getCount() > 0) {
+            restaurantDao.getAll(keyword).collect { emit(it) }
+        } else {
+            getRestaurantsFromRemote().collect { emit(it) }
         }
     }.flowOn(Dispatchers.IO)
 
     private fun getRestaurantsFromRemote() = flow<List<Restaurant>> {
+        currentCoroutineContext().ensureActive()
         val result = ibarraFoodApi.getRestaurants()
-        when(result.isSuccessful) {
-            true -> {
-                result.body()?.let {
-                    restaurantDao.insertList(it.toList()).also {
-                        restaurantDao.getAll()
-                    }
-                } ?: emit(emptyList())
-            }
-            else -> { emit(emptyList())}
+        if (result.isSuccessful) {
+            result.body()?.let {
+                restaurantDao.insertList(it.toList()).also {
+                    restaurantDao.getAll().collect { emit(it) }
+                }
+            } ?: emit(emptyList())
+        } else{
+            emit(emptyList())
         }
     }.flowOn(Dispatchers.IO)
 
