0c94eb435f12e4d98ae2bfcc8b99bb6b164ba2d0
João Ibarra <joao.ibarra@gmail.com>
2021-03-07 12:33:16 +0100

    Feature/favorite restaurant (#6)
    * Adding basic config
    
    * Adding gradle config
    
    * Change gitignore to fix Actions
    
    * Adding gradlew and gradlew.bat
    
    * Adding gradle wrapper
    
    * Favorite button

--- app/src/main/java/com/joaoibarra/food/ui/common/BindingUtils.kt
+++ app/src/main/java/com/joaoibarra/food/ui/common/BindingUtils.kt
@@ -1,11 +1,21 @@
 package com.joaoibarra.food.ui.common
 
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.joaoibarra.food.R
+
 
 @BindingAdapter(value = ["setAdapter"])
 fun RecyclerView.bindRecyclerViewAdapter(adapter: RecyclerView.Adapter<*>?) {
     this.run {
         this.adapter = adapter
     }
+}
+
+@BindingAdapter("app:favIcon")
+fun favIcon(v: AppCompatImageView, isFavorite: Boolean) {
+    val drawableRes: Int =
+        if (isFavorite) R.drawable.ic_favorite_black_48dp else R.drawable.ic_favorite_border_black_48dp
+    v.setImageResource(drawableRes)
 }
